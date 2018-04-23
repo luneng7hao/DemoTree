b00064e7e3e2a863257f9ed689528ee7f37b0cb2
Asion <670364450@qq.com>
2018-04-23 14:17:15 +0800

    新增下拉刷新的moudle

--- app/src/main/java/com/demotree/MainActivity.java
+++ app/src/main/java/com/demotree/MainActivity.java
@@ -1,15 +1,29 @@
 package com.demotree;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import com.asion.pulltorefresh.RefreshView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    RefreshView refreshableView;
+    ArrayAdapter<String> adapter;
+    ListView listView;
+    RecyclerView recyclerview;
+    private static int NUM = 30;
+    String[] items = new String[NUM];
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,6 +53,22 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+        for (int i = 0; i < items.length; i++) {
+            items[i] = "列表项" + i;
+        }
+
+        refreshableView = (RefreshView) findViewById(R.id.refreshable_view);
+
+        listView = (ListView) findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+        refreshableView.setOnRefreshListener(new RefreshView.PullToRefreshListener() {
+            @Override
+            public void onRefresh() {
+                SystemClock.sleep(3000);
+                refreshableView.finishRefreshing();
+            }
+        }, 0);
+    }
 }
